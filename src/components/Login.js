702bc1051173583f1cf4347fd82f6c359b4003b3
ajgogia <ajgogia@deloitte.com>
2025-03-28 01:31:31 +0530

    validators

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { validators } from '../utils/validators'
 
 const Login = () => {
-  const [signIn, setSignIn] = useState(true)
+  let [signIn, setSignIn] = useState(true);
+  let [errorMessage, setErrorMessage] = useState(null)
+  const email = useRef(null);
+  const password = useRef(null)
 
   function toggleSignIn() {
     setSignIn(!signIn)
+  }
+
+  function signInClicked() {
+    const message = validators(email.current.value, password.current.value)
+    setErrorMessage(message)
   }
 
   return (
@@ -16,9 +25,10 @@ const Login = () => {
       </div>
       <form className='absolute mx-auto left-0 right-0 bg-black w-3/12 flex-col text-center h-auto rounded-xl mt-44 bg-opacity-80'>
         <h1 className='text-white text-3xl align text-left ml-8 py-2'>{signIn ? 'Sign In' : 'Sign Up'}</h1>
-        <input type='text' className='m-2 p-3 w-9/12 text-center bg-black border border-white rounded-sm' placeholder='Email or mobile number'></input>
-        <input type='password' className='m-2 p-3 w-9/12 text-center bg-black border border-white rounded-sm' placeholder='Password'></input>
-        <button type='button' className='m-2 p-2 w-9/12 text-center rounded-lg bg-red-600 text-white'>{signIn ? 'Sign In' : 'Sign Up'}</button>
+        <input ref={email} type='text' className='m-2 p-3 w-9/12 text-center bg-black border border-white text-white rounded-sm' placeholder='Email or mobile number'></input>
+        <input ref={password} type='password' className='m-2 p-3 w-9/12 text-center bg-black border border-white text-white rounded-sm' placeholder='Password'></input>
+        <p className='text-red-600 font-bold my-4 mx-auto w-9/12 text-left'>{errorMessage}</p>
+        <button type='button' className='m-2 p-2 w-9/12 text-center rounded-lg bg-red-600 text-white' onClick={signInClicked}>{signIn ? 'Sign In' : 'Sign Up'}</button>
         {signIn && <p className='text-white'>Or</p>}
         {signIn && <button type='button' className='m-2 p-2 w-9/12 text-center rounded-lg bg-gray-400 text-white'>Use a sign-in code</button>}
         <p className='text-white block m-4'>{signIn ? 'New to Netflix?' : 'Already a User?'} <a className='underline cursor-pointer' onClick={toggleSignIn}>{signIn ? 'Sign-up' : 'Sign-in'}</a> Now!</p>
