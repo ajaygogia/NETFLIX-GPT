fcf463993dc61cfc06e91c2f2f756e2a60a0cb29
ajgogia <ajgogia@deloitte.com>
2025-03-30 12:38:44 +0530

    code cleanup

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { API_OPTIONS } from '../utils/constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTrailerVideo } from '../slices/moviesSlice';
+import React from 'react'
+import {  useSelector } from 'react-redux';
+import useTrailerVideo from '../hooks/useTrailerVideo.js';
 
 const VideoBackground = ({ id }) => {
-  const dispatch = useDispatch()
+  useTrailerVideo(id)
   let trailerId = useSelector(Store => Store?.movies?.trailerVideo)
-  async function playVideo() {
-    const data = await fetch('https://api.themoviedb.org/3/movie/' + id + '/videos?language=en-US', API_OPTIONS)
-    const json = await data.json()
-    let index = json.results.find(video => video.type == 'Trailer')
-    let video_key = index.key ?? json.results[0].key
-    dispatch(setTrailerVideo(video_key))
-  }
-  useEffect(() => {
-    playVideo()
-  }, [])
 
   return (
     <div>
---
+++ src/hooks/useTrailerVideo.js
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { API_OPTIONS } from "../utils/constants"
+import { setTrailerVideo } from "../slices/moviesSlice"
+
+const useTrailerVideo = (id) =>{
+    const dispatch = useDispatch()
+    async function playVideo() {
+      const data = await fetch('https://api.themoviedb.org/3/movie/' + id + '/videos?language=en-US', API_OPTIONS)
+      const json = await data.json()
+      let index = json.results.find(video => video.type == 'Trailer')
+      let video_key = index.key ?? json.results[0].key
+      dispatch(setTrailerVideo(video_key))
+    }
+    useEffect(() => {
+      playVideo()
+    }, [])
+}
+
+export default useTrailerVideo
